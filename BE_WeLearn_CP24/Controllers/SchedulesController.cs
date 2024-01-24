ff4261d130f8e5da6cb04d2fcdbd54b29adc490a
antitank19 <80617228+antitank19@users.noreply.github.com>
2024-01-24 17:36:29 +0700

    Add Schedule Controller

---
+++ BE_WeLearn_CP24/Controllers/SchedulesController.cs
@@ -0,0 +1,42 @@
+﻿using API.Extension.ClaimsPrinciple;
+using API.SwaggerOption.Const;
+using API.SwaggerOption.Endpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.DTOs;
+using ServiceLayer.Services.Interface;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchedulesController : ControllerBase
+    {
+        private readonly IServiceWrapper services;
+
+        public SchedulesController(IServiceWrapper services)
+        {
+            this.services = services;
+        }
+
+        //GET: api/Schedules/Group/groupId
+        [SwaggerOperation(
+            Summary = SchedulesEndpoints.GetSchedulesForGroup
+        )]
+        [Authorize(Roles = Actor.Student)]
+        [HttpGet("Group/{groupId}")]
+        public async Task<IActionResult> GetSchedulesForGroup(int groupId)
+        {
+            int studentId = HttpContext.User.GetUserId();
+            bool isJoined = await services.Groups.IsStudentJoiningGroupAsync(studentId, groupId);
+            if (!isJoined)
+            {
+                return Unauthorized("Bạn không phải là thành viên nhóm này");
+            }
+            IQueryable<ScheduleGetDto> mapped = services.Meetings.GetSchedulesForGroup(groupId);
+            return Ok(mapped);
+        }
+    }
+}

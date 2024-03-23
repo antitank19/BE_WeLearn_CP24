404517be4be65a8aaf730d8413a09965caeb9af9
antitank19 <80617228+antitank19@users.noreply.github.com>
2024-03-23 23:43:07 +0700

    Temp

--- BE_WeLearn_CP24/Controllers/ReportsController.cs
+++ BE_WeLearn_CP24/Controllers/ReportsController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Extension.ClaimsPrinciple;
 using APIExtension.Validator;
+using DataLayer.DbObject;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -48,6 +49,95 @@ namespace API.Controllers
             try
             {
                 var mapped = services.Reports.GetUnresolvedReportList<ReportGetListDto>();
+                if (mapped == null || !mapped.Any())
+                {
+                    valResult.Add("Không có báo cáo nào hết", ValidateErrType.NotFound);
+                    return NotFound(valResult);
+                }
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                valResult.Add(ex.ToString());
+                return BadRequest(valResult);
+            }
+        }
+
+
+        //[Authorize]
+        [HttpGet("Account")]
+        public async Task<IActionResult> GetUresolvedReportedAccount()
+        {
+            ValidatorResult valResult = new ValidatorResult();
+            try
+            {
+                var mapped = services.Reports.GetReportedAccountList<Account>();
+                if (mapped == null || !mapped.Any())
+                {
+                    valResult.Add("Không có báo cáo nào hết", ValidateErrType.NotFound);
+                    return NotFound(valResult);
+                }
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                valResult.Add(ex.ToString());
+                return BadRequest(valResult);
+            }
+        }
+
+        //[Authorize]
+        [HttpGet("Group")]
+        public async Task<IActionResult> GetUresolvedReportedGroup()
+        {
+            ValidatorResult valResult = new ValidatorResult();
+            try
+            {
+                var mapped = services.Reports.GetReportedAccountList<Group>();
+                if (mapped == null || !mapped.Any())
+                {
+                    valResult.Add("Không có báo cáo nào hết", ValidateErrType.NotFound);
+                    return NotFound(valResult);
+                }
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                valResult.Add(ex.ToString());
+                return BadRequest(valResult);
+            }
+        }
+
+        //[Authorize]
+        [HttpGet("Discussion")]
+        public async Task<IActionResult> GetUresolvedReportedDiscussion()
+        {
+            ValidatorResult valResult = new ValidatorResult();
+            try
+            {
+                var mapped = services.Reports.GetReportedAccountList<Discussion>();
+                if (mapped == null || !mapped.Any())
+                {
+                    valResult.Add("Không có báo cáo nào hết", ValidateErrType.NotFound);
+                    return NotFound(valResult);
+                }
+                return Ok(mapped);
+            }
+            catch (Exception ex)
+            {
+                valResult.Add(ex.ToString());
+                return BadRequest(valResult);
+            }
+        }
+
+        //[Authorize]
+        [HttpGet("File")]
+        public async Task<IActionResult> GetUresolvedReportedFile()
+        {
+            ValidatorResult valResult = new ValidatorResult();
+            try
+            {
+                var mapped = services.Reports.GetReportedAccountList<DocumentFile>();
                 if (mapped == null || !mapped.Any())
                 {
                     valResult.Add("Không có báo cáo nào hết", ValidateErrType.NotFound);
--- ServiceLayer/Services/Interface/Db/IReportService.cs
+++ ServiceLayer/Services/Interface/Db/IReportService.cs
@@ -12,6 +12,10 @@ namespace ServiceLayer.Services.Interface.Db
     {
         public IQueryable<T> GetReportList<T>();
         public IQueryable<T> GetUnresolvedReportList<T>();
+        public IQueryable<T> GetReportedAccountList<T>();
+        public IQueryable<T> GetReportedGroupList<T>();
+        public IQueryable<T> GetReportedFileList<T>();
+        public IQueryable<T> GetReportedDiscussionList<T>();
         public Task ResolveReport(int reportId, RequestStateEnum newState);
         public Task CreateReport(ReportCreateDto dto, int reporterId);
         public Task<bool> IsReportExist(int reportId);
